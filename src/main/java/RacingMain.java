116cb27322b720b0f7971a90f3b0077a5d58dc0a
20011730 <lsh76080m@naver.com>
2024-03-31 22:20:25 +0900

    우승자 출력

--- src/main/java/RacingMain.java
+++ src/main/java/RacingMain.java
@@ -1,10 +1,6 @@
 import controller.RacingController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
 
 public class RacingMain {
 
